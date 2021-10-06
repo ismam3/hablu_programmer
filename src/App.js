edd5f56f0f7e9d09a9674d074d2885e7b93624f1
ismam3 <ismamlabib3@gmail.com>
2021-10-06 22:47:35 +0600

    Code editor is added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import Landing from './pages/Landing';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import CodeEditor from './pages/CodeEditor';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Switch>
-          <Route path="/" component={Landing}/>
+          <Route exact path="/" component={Landing}/>
+          <Route exact path="/codeEditor" component={CodeEditor}/>
         </Switch>
       </BrowserRouter>
     </div>
